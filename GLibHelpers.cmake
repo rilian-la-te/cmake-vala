3c2bfd689bb61431a807f268e8543dd8a3903080
Konstantin <ria.freelander@gmail.com>
2017-09-20 11:37:32 +0300

    Add gdbus-codegen to GLibHelpers

--- GLibHelpers.cmake
+++ GLibHelpers.cmake
@@ -1,5 +1,5 @@
 macro(add_glib_marshal outsources outincludes name prefix)
-  find_package(GLibTools REQUIRED)
+  find_package(GLibTools REQUIRED QUIET)
   add_custom_command(
     OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${name}.h"
     COMMAND ${GLIB-GENMARSHAL_EXECUTABLE} --header "--prefix=${prefix}"
@@ -20,6 +20,7 @@ macro(add_glib_marshal outsources outincludes name prefix)
 endmacro(add_glib_marshal)
 
 macro(add_glib_enumtypes outsources outheaders name)
+    find_package(GLibTools REQUIRED QUIET)
     set(files ${ARGN})
 	add_custom_command(
 	  OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${name}.h"
@@ -43,3 +44,58 @@ macro(add_glib_enumtypes outsources outheaders name)
 	list(APPEND ${outheaders} "${CMAKE_CURRENT_BINARY_DIR}/${name}.h")
 endmacro(add_glib_enumtypes)
 
+#.rst:
+#.. command:: add_gdbus_codegen
+#
+#  Generates C code and header file from XML service description, and
+#  appends the sources to the SOURCES list provided.
+#
+#    add_gdbus_codegen(<SOURCES> <NAME> <PREFIX> <SERVICE_XML> [NAMESPACE])
+#
+#  For example:
+#
+#  .. code-block:: cmake
+#
+#   set(MY_SOURCES foo.c)
+#
+#   add_gdbus_codegen(MY_SOURCES
+#     dbus-proxy
+#     org.freedesktop
+#     org.freedesktop.DBus.xml
+#     )
+#
+function(ADD_GDBUS_CODEGEN _SOURCES _NAME _PREFIX SERVICE_XML)
+  find_package(GLibTools REQUIRED QUIET)
+  set(_options ALL)
+  set(_oneValueArgs NAMESPACE)
+
+  cmake_parse_arguments(_ARG "${_options}" "${_oneValueArgs}" "" ${ARGN})
+
+  get_filename_component(_ABS_SERVICE_XML ${SERVICE_XML} ABSOLUTE)
+
+  set(_NAMESPACE "")
+  if(_ARG_NAMESPACE)
+    set(_NAMESPACE "--c-namespace=${_ARG_NAMESPACE}")
+  endif()
+
+  set(_OUTPUT_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/${_NAME}")
+  set(_OUTPUT_FILES "${_OUTPUT_PREFIX}.c" "${_OUTPUT_PREFIX}.h")
+
+  # for backwards compatibility
+  set("${_SOURCES}_SOURCES" "${_OUTPUT_FILES}" PARENT_SCOPE)
+
+
+  list(APPEND ${_SOURCES} ${_OUTPUT_FILES})
+  set(${_SOURCES} ${${_SOURCES}} PARENT_SCOPE)
+
+  add_custom_command(
+    OUTPUT ${_OUTPUT_FILES}
+    COMMAND "${GDBUS-CODEGEN_EXECUTABLE}"
+        --interface-prefix ${_PREFIX}
+        --generate-c-code="${_NAME}"
+        ${_NAMESPACE}
+        ${_ABS_SERVICE_XML}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    DEPENDS ${_ABS_SERVICE_XML}
+    )
+endfunction()
